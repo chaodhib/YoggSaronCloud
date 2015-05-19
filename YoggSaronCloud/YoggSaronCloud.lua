6b38b322b2e4d6889c2a53903861f250744b3b21
Chaouki Dhib <chaodhib@gmail.com>
2015-05-19 06:35:32 +0200

    fixed a bug where my 2 addons would share the isActive variable (meaning that either both were active or both were disabled) which was not intended.

--- YoggSaronCloud/YoggSaronCloud.lua
+++ YoggSaronCloud/YoggSaronCloud.lua
@@ -14,30 +14,30 @@ local L = MyLocalizationTable;
 -----------------------------------------------------------------------------------
 -- Initializing variables
 -----------------------------------------------------------------------------------
-if (isActive == nil) then isActive = true; end
-if (channel == nil) then channel = "RAID"; end
+if (YSC_isActive == nil) then YSC_isActive = true; end
+if (YSC_channel == nil) then YSC_channel = "RAID"; end
 -----------------------------------------------------------------------------------
 
 local YoggSaronCloud = CreateFrame('Frame')
 
 local function OnEvent(self, event, ...)
 
-	if not isActive then return end
+	if not YSC_isActive then return end
 	if GetNumRaidMembers()==0 then return end
 	if select(2,...) ~= "Ominous Cloud"  then return end
 
 	local idiotName=select(5,...)
 	
-	SendChatMessage("{Triangle}"..idiotName.." has spawned a guardian! Take care!{Triangle}", channel)
+	SendChatMessage("{Triangle}"..idiotName.." has spawned a guardian! Take care!{Triangle}", YSC_channel)
 end
 
 -- Slash Commands
 SLASH_YoggSaronCloud1 = '/ysc'; 
 function SlashCmdList.YoggSaronCloud(msg, editbox)
-	if (msg == "on") then isActive = true; print("YoggSaronCloud is now: Active"); return end
-	if (msg == "off") then isActive = false; print("YoggSaronCloud is now: Inactive"); return end
-	if (msg == "raid") then channel = "RAID"; print("Channel is set to: Raid"); return end
-	if (msg == "warning") then channel = "RAID_WARNING"; print("Channel is set to: Raid Warning"); return end
+	if (msg == "on") then YSC_isActive = true; print("YoggSaronCloud is now: Active"); return end
+	if (msg == "off") then YSC_isActive = false; print("YoggSaronCloud is now: Inactive"); return end
+	if (msg == "raid") then YSC_channel = "RAID"; print("channel is set to: Raid"); return end
+	if (msg == "warning") then YSC_channel = "RAID_WARNING"; print("channel is set to: Raid Warning"); return end
 
  print("----------------------------------------------------------");
  print("YoggSaronCloud commands");
@@ -47,10 +47,10 @@ function SlashCmdList.YoggSaronCloud(msg, editbox)
  print("/ysc raid- change the output channel to raid");
  print("/ysc warning- change the output channel to raid warning");
  print("----------------------------------------------------------");
-  if (isActive == true) then print("YoggSaronCloud is currently: Active"); end 
-  if (isActive == false) then print("YoggSaronCloud is currently: Inactive"); end
-  if (channel == "RAID") then print("Channel is set to: Raid"); end
-  if (channel == "RAID_WARNING") then print("Channel is set to: Raid Warning"); end
+  if (YSC_isActive == true) then print("YoggSaronCloud is currently: Active"); end 
+  if (YSC_isActive == false) then print("YoggSaronCloud is currently: Inactive"); end
+  if (YSC_channel == "RAID") then print("channel is set to: Raid"); end
+  if (YSC_channel == "RAID_WARNING") then print("channel is set to: Raid Warning"); end
  print("----------------------------------------------------------");
 end
 
